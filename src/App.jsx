a98bb4a28c58b6c7370d059cddbcee3269213cef
anastasia-tarasova26 <nastya.tarasova2682@gmail.com>
2024-06-08 09:24:51 +0300

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,92 @@
 import "./App.css";
+import axios from "axios";
 import React from "react";
-import ContactFrom from "./components/ContactForm/ContactForm";
-import ContacList from "./components/ContactList/ContactList";
-import SearchForm from "./components/SearchBox/SearchBox";
 import { useState } from "react";
 import { useEffect } from "react";
 import * as Yup from "yup";
 import { Formik, Form, Field } from "formik";
-import initialContacts from "./contacts.json";
-import SearchBox from "./components/SearchBox/SearchBox";
-
-const contactsData = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+import SearchBar from "./components/SearchBar/SearchBar";
+import fetchPhotos from "./fetchPhotos";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import ImageModal from "./components/ImageModal/ImageModal";
+import LoadMoreBtn from "./components/Loader/LoadMoreBtn";
+import ErrorMessage from "./ErrorMassage/ErrorMessage";
+import Loader from "./components/LoadMoreBtn/Loader";
 
 const App = () => {
-  const [contact, setContact] = useState(() => {
-    const savedObject = window.localStorage.getItem("contacts");
-    if (savedObject !== null) {
-      return JSON.parse(savedObject);
-    }
-    return contactsData;
-  });
-  const [searchValue, setSearchValue] = useState("");
-
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState("");
+  const [photos, setPhotos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [modalFilter, setModalFilter] = useState();
+  const [contentForModal] = photos.filter((photo) => photo.id === modalFilter);
   useEffect(() => {
-    window.localStorage.setItem("contacts", JSON.stringify(contact));
-  }, [contact]);
+    async function searchPictures() {
+      if (query === "") {
+        return;
+      }
+      setLoading(true);
+      try {
+        const apiRequest = await fetchPhotos(query, page);
+        setPhotos((prevState) => [...prevState, ...apiRequest]);
+        setError(false);
+      } catch (error) {
+        setLoading(false);
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    searchPictures();
+  }, [query, page]);
 
-  const filterContacts = contact.filter((contact) =>
-    contact.name.toLowerCase().includes(searchValue.toLowerCase())
-  );
-
-  function addContact(contactObj) {
-    setContact((prevValue) => {
-      return [...prevValue, contactObj];
-    });
+  function onFormSubmit(searchedWord) {
+    if (query.toLowerCase() !== searchedWord.toLowerCase()) {
+      setPhotos([]);
+      setQuery(searchedWord);
+    }
+    setPage(1);
   }
 
-  function addFilter(currentFilter) {
-    setSearchValue(currentFilter);
+  function handleLoadMoreBtnClick() {
+    setPage((prevState) => prevState + 1);
+    setLoading(true);
   }
 
-  function deleteContact(contactId) {
-    setContact((prevContact) =>
-      prevContact.filter((contact) => contact.id !== contactId)
-    );
+  function openModal() {
+    setIsOpen(true);
   }
 
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function createModalContent(id) {
+    setModalFilter(id);
+  }
   return (
-    <div>
-      <h1>Phonebook</h1>
-      <ContactFrom addContact={addContact} />
-      <SearchBox value={searchValue} addFilter={addFilter} />
-      <ContacList contacts={filterContacts} onDelete={deleteContact} />
-    </div>
+    <>
+      <SearchBar onFormSubmit={onFormSubmit} />
+      <main>
+        <ImageGallery
+          modalContent={createModalContent}
+          openModal={openModal}
+          photos={photos}
+        />
+        {error && <ErrorMessage />}
+        {loading && <Loader />}
+        {photos.length > 0 && !loading && (
+          <LoadMoreBtn handleLoadMoreBtnClick={handleLoadMoreBtnClick} />
+        )}
+        <ImageModal
+          modalContent={contentForModal}
+          isOpen={modalIsOpen}
+          closeModal={closeModal}
+        />
+      </main>{" "}
+    </>
   );
 };
 
